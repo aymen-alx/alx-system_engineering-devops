dacc9579296a0062d3a36da9f2aa6eff4ee396e0
Aymen <aymenabduseid10@gmail.com>
2023-04-04 22:05:26 +0300

    fix update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,9 @@
 # Task 2
 
-exec {'update':
-    command     => '/usr/bin/apt-get update',
-}
-
-package { 'nginx':
-    ensure  => 'installed',
-    require => Exec['update']
-}
-
-
 exec { 'command':
-    command  => 'sudo sed -i "/listen 80 default_server;
-    /a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
-    service nginx restart',
-    provider => shell,
-    require  => Exec['nginx']
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
