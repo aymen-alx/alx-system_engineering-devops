21690ad508c79cf46ff555f41a1385b31a03d09f
Aymen <aymenabduseid10@gmail.com>
2023-04-24 21:33:11 +0300

    task0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""  """
+import sys
+import requests
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    link = 'https://jsonplaceholder.typicode.com/'
+    name = requests.get('{}{}{}'.format(
+        link, 'users/', user_id)).json().get('name')
+    todos = requests.get('{}{}{}'.format(
+        link, 'todos?userId=', user_id)).json()
+    total_tasks = len(todos)
+
+    done_tasks = 0
+    for task in todos:
+        if task.get('completed') is True:
+            done_tasks += 1
+    print('Employee {} is done with tasks({}/{}):'.format(
+        name, done_tasks, total_tasks))
+    for task in todos:
+        if task.get('completed') is True:
+            print('    ', task.get('title'))
