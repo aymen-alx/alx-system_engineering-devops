f08946d1a25be8576a7574e3872c12d18a075059
Aymen <aymenabduseid10@gmail.com>
2023-04-24 21:43:55 +0300

    task1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     user_id = sys.argv[1]
     link = 'https://jsonplaceholder.typicode.com/'
     name = requests.get('{}{}{}'.format(
-        link, 'users/', user_id)).json().get('name')
+        link, 'users/', user_id)).json().get('username')
     todos = requests.get('{}{}{}'.format(
         link, 'todos?userId=', user_id)).json()
     total_tasks = len(todos)
