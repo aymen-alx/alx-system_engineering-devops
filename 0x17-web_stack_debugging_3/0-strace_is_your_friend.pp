d882a224b89b58800845752af2744818347e511f
Aymen <aymenabduseid10@gmail.com>
2023-05-10 22:05:42 +0300

    task0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Comment
+
+file { '/var/www/html/wp-includes/class-wp-locale.phpp':
+    ensure => present,
+    source => '/var/www/html/wp-includes/class-wp-locale.php',
+    replace => true,
+}
