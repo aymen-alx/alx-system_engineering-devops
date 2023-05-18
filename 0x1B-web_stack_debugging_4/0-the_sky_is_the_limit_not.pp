a95f01216edf7e08d3d7ab327e17dcf72acd74cd
Aymen <aymenabduseid10@gmail.com>
2023-05-18 22:02:44 +0300

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,2 @@
 # Configure nginx limit and restart it
 
-exec { 'replace_ulimit':
-  command => "sed -i 's/^ULIMIT=\"-n 15\"$/ULIMIT=\"-n 4096\"/' /etc/default/nginx",
-}
-
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/',
-}
