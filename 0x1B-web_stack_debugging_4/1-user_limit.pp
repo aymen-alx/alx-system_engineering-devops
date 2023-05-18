4e675a67bb7888165fc5df752af2f3e16c82cb2f
Aymen <aymenabduseid10@gmail.com>
2023-05-18 22:20:33 +0300

    task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,10 +2,10 @@
 
 exec { 'configure hard file':
   path    => '/bin',
-  command => "sed -i 's/5/4096/' /etc/security/limits.conf"
+  command => "sed -i '/holberton hard/s/5/4096/' /etc/security/limits.conf"
 }
 
 exec { 'configure hard file':
   path    => '/bin',
-  command => "sed -i 's/4/4096/' /etc/security/limits.conf"
+  command => "sed -i '/holberton soft/s/4/4096/' /etc/security/limits.conf"
 }
