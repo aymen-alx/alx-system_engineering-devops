9121babbda7946ea0e5dc63b5e040b1ca4a7c069
Aymen <aymenabduseid10@gmail.com>
2023-05-18 21:38:03 +0300

    task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,14 @@
+# Configure nginx limit and restart it
+
+$file_path = '/etc/default/nginx'
+$replacement = 'ULIMIT="-n 4096"'
+
+file { $file_path:
+  ensure  => present,
+#  content => file($file_path).content.gsub(/ULIMIT="-n \d+"/, $replacement),
+}
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
